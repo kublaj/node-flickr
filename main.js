f38eceb7dfc5713ee348384f45aa4671cccba5bf
kublaj <isztan@isztan.eu>
2017-12-06 17:22:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ module.exports = Flickr;
 
 /*++++++++++++++++++++ How to Starts +++++++++++++++++++++++*/
 
-// var keys = {"api_key": "672dab998f10d23c46fc4d4c2dbb8132"}
+ var keys = {"api_key": "672dab998f10d23c46fc4d4c2dbb8132"}
 
-// flickr = new Flickr(keys);
+ flickr = new Flickr(keys);
 
-// flickr.get("photos.search", {"tags":"cat,dogs"}, function(result){
-//     console.log(result.photos);
-// });
+ flickr.get("photos.search", {"tags":"supercell,thunderstorm"}, function(result){
+     console.log(result.photos);
+ });
